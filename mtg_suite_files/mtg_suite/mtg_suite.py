77b914d02a3b001332285a452181f62247076e53
Georgios Filis <108461618+Georgios-Filis@users.noreply.github.com>
2026-03-17 11:22:24 +0200

    Add files via upload

---
+++ mtg_suite_files/mtg_suite/mtg_suite.py
@@ -0,0 +1,26 @@
+import re
+import os
+import sys
+import csv
+import copy
+import time
+import shutil
+import string
+import random
+
+
+def run_mtg_suite():   
+    # Print a new line in the output.
+    print("\n----------------")
+    print("MTG suite - Start")
+    print("----------------")
+    print()
+    print()
+    print("----------------")
+    print("MTG suite - End")
+    print("----------------")
+    print()
+
+
+if __name__ == "__main__":
+    run_mtg_suite()
